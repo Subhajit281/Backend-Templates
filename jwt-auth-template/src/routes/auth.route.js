a597198cf92f325aba8bce189dad4e239a82a84d
Subhajit281 <subhajitsarkar281@gmail.com>
2026-06-14 14:32:01 +0530

    bug fixed

--- jwt-auth-template/src/routes/auth.route.js
+++ jwt-auth-template/src/routes/auth.route.js
@@ -1,12 +1,51 @@
 const router = require('express').Router();
-const {registerUser,loginUser,getProfile , updateUser,deleteUser} = require('../controllers/auth.controller');
-const authMiddleware = require('../middleware/auth.middleware');
-const validateUserReg = require('../middleware/validate.middleware');
 
-router.post('/register',validateUserReg,registerUser);
-router.post('/login',loginUser);
-router.get('/profile',authMiddleware,getProfile);
-router.patch('/update',authMiddleware,updateUser);
-router.delete('/delete',authMiddleware,deleteUser);
+const {
+    registerUser,
+    loginUser,
+    getProfile,
+    updateUser,
+    deleteUser
+} = require('../controllers/auth.controller');
+
+const authMiddleware = require('../middleware/auth.middleware');
+const validate = require('../middleware/validate.middleware');
+
+const {
+    registerSchema,
+    loginSchema,
+    updateProfileSchema
+} = require('../validators/auth.validator');
+
+router.post(
+    '/register',
+    validate(registerSchema),
+    registerUser
+);
+
+router.post(
+    '/login',
+    validate(loginSchema),
+    loginUser
+);
+
+router.get(
+    '/profile',
+    authMiddleware,
+    getProfile
+);
+
+router.patch(
+    '/update',
+    authMiddleware,
+    validate(updateProfileSchema),
+    updateUser
+);
+
+router.delete(
+    '/delete',
+    authMiddleware,
+    deleteUser
+);
 
 module.exports = router;
